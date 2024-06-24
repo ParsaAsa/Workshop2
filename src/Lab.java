ca3321bcefd197ca6a4748b4c3dc5011b332037b
unknown <parsaasa84@gmail.com>
2024-06-24 13:20:25 +0330

    Lab commit2

--- src/Lab.java
+++ src/Lab.java
@@ -13,4 +13,58 @@ public class Lab {
 		this.maxSize = maxSize;
 		students = new Student[maxSize];
 	}
+	public void setMaxSize(int maxSize) {
+		this.maxSize = maxSize;
+	}
+	public void setDayOfWeek(String dayOfWeek) {
+		this.dayOfWeek = dayOfWeek;
+	}
+	public void setTeacherName(String teacherName) {
+		this.teacherName = teacherName;
+	}
+	public void setCurrentSize (int currentSize) {
+		this.currentSize = currentSize;
+	}
+	public void setStudents(Student[] students) {
+		this.students = students;
+	}
+	public void setAvgGrade(double average) {
+		this.avgGrade = average;
+	}
+	public double getAvgGrade() {
+		return this.avgGrade;
+	}
+	public int getCurentSize() {
+		return this.currentSize;
+	}
+	public int getMaxSize() {
+		return this.maxSize;
+	}
+	public String getDayOfWeek() {
+		return this.dayOfWeek;
+	}
+	public Student[] getStudents() {
+		return this.students;
+	}
+	public String getTeacherName() {
+		return this.teacherName;
+	}
+	public void calAvg(){
+		double gradeSum = 0;
+		for (int i = 0; i < this.currentSize; i++)
+			gradeSum += this.students[i].getGrade();
+		System.out.println("Average grade is: " + (gradeSum/this.currentSize));
+	}
+	public void enrollStudent(Student student) {
+		if (this.currentSize == this.maxSize) {
+			System.out.println("You have the maximum number of students");
+			return;
+		}
+		this.students[this.currentSize] = student;
+		this.currentSize++;
+	}
+	public void printLabInfo() {
+		System.out.println("Teacher name: " + this.teacherName + 
+				"\nDay of week: " + this.dayOfWeek + "\nAverage grade is: " + this.avgGrade);
+	}
 }
